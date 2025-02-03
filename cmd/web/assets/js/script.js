de1d93bd4181b0baf06c0eff877cd9b060a7b446
coleYab <yabume123@gmail.com>
2025-02-03 08:24:59 +0000

    add: fixing the authorization on htmx

--- cmd/web/assets/js/script.js
+++ cmd/web/assets/js/script.js
@@ -10,6 +10,14 @@ alert(initData || "Init data is not found")
 
 // always on request set the authorization token with htmx
 htmx.on("htmx:configRequest", (e)=> {
-  e.details.headers["Authorization"] = `tma ${initData}`
-  console.log("the init data is set to be", e.detail.headers["Authorization"])
+  e.detail.headers["AUTH"] = `tma ${initData}`
+  console.log("the init data is set to be", e.detail.headers["AUTH"])
 })
+
+// 2025/02/03 08:13:51 sign is missing
+// 2025/02/03 08:13:51 GET /assets/js/script.js 200 27.581µs
+// 2025/02/03 08:13:51 Logging the init data sent with the request:  
+// 2025/02/03 08:13:51 sign is missing
+// 2025/02/03 08:13:51 GET /assets/images/rank.svg 200 26.33µs
+// 2025/02/03 08:13:51 Logging the init data sent with the request:  
+// 2025/02/03 08:13:51 sign is missing
